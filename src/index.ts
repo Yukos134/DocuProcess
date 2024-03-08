c638c0082a0e07c5da8a7fc9b11abadf8e48b6a8
Georgy Tkachuk <crs.georgy.tkachuk@hubinternational.com>
2024-03-08 10:42:29 +0400

    feat: add main index ts

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import cluster from "cluster";
+import os from "os";
+
+const numCPUs = os.cpus().length;
+
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died`);
+  });
+} else {
+  console.log(`Worker ${process.pid} started`);
+  const draftAPI = require("./draftAPI");
+  const documentAPI = require("./documentAPI");
+}
